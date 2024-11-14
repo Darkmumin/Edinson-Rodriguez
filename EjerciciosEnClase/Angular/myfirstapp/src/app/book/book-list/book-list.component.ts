f9afc1cc7f0a5d88c7096c931f8cb09a11d3d5e0
Edinson Rodriguez <edi.rodriguez.saza@gmail.com>
2024-11-13 22:45:41 -0500

    se agrega los libros al las lista de componentes de libros

--- EjerciciosEnClase/Angular/myfirstapp/src/app/book/book-list/book-list.component.ts
+++ EjerciciosEnClase/Angular/myfirstapp/src/app/book/book-list/book-list.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../book';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css'
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit {
 
+  books: Array<Book>=[];
+  constructor(){}
+  ngOnInit() {
+    
+  }
 }
