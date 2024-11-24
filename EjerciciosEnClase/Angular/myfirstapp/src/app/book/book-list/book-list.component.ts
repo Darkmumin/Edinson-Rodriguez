da259f5ae76e6f5e9efc0516716b6266f602b251
Edinson Rodriguez <edi.rodriguez.saza@gmail.com>
2024-11-24 11:39:56 -0500

    se envia el parametro input al componente detalle

--- EjerciciosEnClase/Angular/myfirstapp/src/app/book/book-list/book-list.component.ts
+++ EjerciciosEnClase/Angular/myfirstapp/src/app/book/book-list/book-list.component.ts
@@ -8,13 +8,18 @@ import { BookService } from '../book.service';
   styleUrl: './book-list.component.css'
 })
 export class BookListComponent implements OnInit {
-
+  selectedBook!: Book;
+  selected: Boolean = false;
   books: Array<Book>=[];
   constructor(private bookService:BookService){}
   getBooks():void{
     this.bookService.getBooks().subscribe((books)=>{
       this.books=books;
     });
+  }
+  onSelected(book:Book): void{
+    this.selected = true;
+    this.selectedBook = book;
   }
   ngOnInit():void {
     this.getBooks();    
